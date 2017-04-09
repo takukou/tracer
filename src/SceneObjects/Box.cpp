858286dadb91bacecf5fa10da63f3b3c27b88f0e
Lam Jouq Hang Hendersen <hendersenlam@gmail.com>
2017-04-10 00:48:30 +0800

    Fixed Box Intersection

--- src/SceneObjects/Box.cpp
+++ src/SceneObjects/Box.cpp
@@ -5,47 +5,70 @@
 
 bool Box::intersectLocal( const ray& r, isect& i ) const
 {
-	// YOUR CODE HERE:
-    // Add box intersection code here.
-	// it currently ignores all boxes and just returns false.
 	i.obj = this;
+
 	double tFar = numeric_limits<double>::infinity(), tNear = -tFar;
 	
 	vec3f dir = r.getDirection();
 	vec3f org = r.getPosition();
 
-	for (int axis = 0; axis < 3; axis++) {
+	double aMin = -0.5, aMax = 0.5;
+
+	int tNearAxis;
+
+	for (int axis = 0; axis < 3; ++axis) {
 		double aDir = dir[axis];
 		double aOrg = org[axis];
-		double aMin = this->bounds.min[axis];
-		double aMax = this->bounds.max[axis];
-		if (aDir == 0) {
+		if (abs(aDir) < RAY_EPSILON) {
 			if (aOrg < aMin || aOrg > aMax)
 				return false;
 		}
-		else {
-			double T1 = (aMin - aOrg) / aDir;
-			double T2 = (aMax - aOrg) / aDir;
+		
+		double T1 = (aMin - aOrg) / aDir;
+		double T2 = (aMax - aOrg) / aDir;
 			
-			if (T1 > T2) { // since T1 intersection with near plane
-				double temp = T1;
-				T1 = T2;
-				T2 = temp;
-			}
-
-			if (T1 > tNear)
-				tNear = T1; // want largest tNear
-
-			if (T2 < tFar)
-				tFar = T2; // want smallest tFar
-			
-			if (tNear > tFar)
-				return false; // box is missed
-
-			if (tFar < 0)
-				return false; // box is behind ray
+		if (T1 > T2) { // since T1 intersection with near plane
+			double temp = T1;
+			T1 = T2;
+			T2 = temp;
 		}
+
+		if (T1 > tNear) {
+			tNear = T1; // want largest tNear
+			tNearAxis = axis;
+		}
+
+		if (T2 < tFar) {
+			tFar = T2; // want smallest tFar
+		}
+			
+		if (tNear > tFar)
+			return false; // box is missed
+
+		if (tFar < RAY_EPSILON)
+			return false; // box is behind ray
 	}
 
+	i.setT(tNear);
+	switch (tNearAxis) {
+		case (0):
+			if (dir[tNearAxis] < 0)
+				i.setN(vec3f(1.0, 0.0, 0.0));
+			else
+				i.setN(vec3f(-1.0, 0.0, 0.0));
+			break;
+		case (1):
+			if (dir[tNearAxis] < 0)
+				i.setN(vec3f(0.0, 1.0, 0.0));
+			else
+				i.setN(vec3f(0.0, -1.0, 0.0));
+			break;
+		case (2):
+			if (dir[tNearAxis] < 0)
+				i.setN(vec3f(0.0, 0.0, 1.0));
+			else
+				i.setN(vec3f(0.0, 0.0, -1.0));
+			break;
+	}
 	return true;
 }
