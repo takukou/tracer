b566ebf875cb567da70ad77bbfb99daed68c1ba1
Lam Jouq Hang Hendersen <hendersenlam@gmail.com>
2017-04-10 01:13:45 +0800

    Fixed diffuse and distance attenuation

--- src/scene/light.cpp
+++ src/scene/light.cpp
@@ -13,13 +13,16 @@ vec3f DirectionalLight::shadowAttenuation( const vec3f& P ) const
 {
 	vec3f d = getDirection(P).normalize();
 	isect i;
-	ray R(P, d);
-	if (scene->intersect(R, i)) {
-		return vec3f(0.0, 0.0, 0.0);
+	ray R = ray(P, d);
+	vec3f result = getColor(P);
+	while(scene->intersect(R, i)) {
+		// transparent
+		if (i.getMaterial().kt.iszero())
+			return vec3f(0.0, 0.0, 0.0);
+		R = ray(R.at(i.t), d);
+		result = prod(result, i.getMaterial().kt);
 	}
-	else {
-		return vec3f(1.0, 1.0, 1.0);
-	}
+	return result;
 }
 
 vec3f DirectionalLight::getColor( const vec3f& P ) const
@@ -40,8 +43,8 @@ double PointLight::distanceAttenuation(const vec3f& P) const
 	// point P.  For now, I assume no attenuation and just return 1.0
 	double d = (P - position).length();
 	double d2 = (P - position).length_squared();
-	
-	return minimum(1.0, 1.0/(constant_atten + linear_atten * d + quadratic_atten * d2));
+	double coeff = constant_atten + linear_atten * d + quadratic_atten * d2;
+	return minimum(1.0, 1.0 / coeff);
 }
 
 vec3f PointLight::getColor(const vec3f& P) const
@@ -66,19 +69,16 @@ vec3f PointLight::shadowAttenuation(const vec3f& P) const
 {
 	vec3f d = getDirection(P).normalize();
 	isect i;
-	ray R(P, d);
-	if (scene->intersect(R, i)) {
-		double Q = (position - P).length();
-		if (Q > i.t) {
+	ray R = ray(P, d);
+	vec3f result = getColor(P);
+	while (scene->intersect(R, i)) {
+		// transparent
+		if (i.getMaterial().kt.iszero())
 			return vec3f(0.0, 0.0, 0.0);
-		}
-		else {
-			return vec3f(1.0, 1.0, 1.0);
-		}
+		R = ray(R.at(i.t), d);
+		result = prod(result, i.getMaterial().kt);
 	}
-	else {
-		return vec3f(1.0, 1.0, 1.0);
-	}
+	return result;
 }
 double AmbientLight::distanceAttenuation(const vec3f& P) const
 {
@@ -94,7 +94,7 @@ vec3f AmbientLight::getColor(const vec3f& P) const
 vec3f AmbientLight::getDirection(const vec3f& P) const
 {
 	// no direction, illuminates surrounding
-	return vec3f(0.0, 0.0, 0.0);
+	return vec3f(1, 1, 1);
 }
 
 
--- src/scene/material.cpp
+++ src/scene/material.cpp
@@ -6,34 +6,24 @@
 // the color of that point.
 vec3f Material::shade( Scene *scene, const ray& r, const isect& i ) const
 {
-	// YOUR CODE HERE
-
-	// For now, this method just returns the diffuse color of the object.
-	// This gives a single matte color for every distinct surface in the
-	// scene, and that's it.  Simple, but enough to get you started.
-	// (It's also inconsistent with the phong model...)
-
-	// Your mission is to fill in this method with the rest of the phong
-	// shading model, including the contributions of all the light sources.
-    // You will need to call both distanceAttenuation() and shadowAttenuation()
-    // somewhere in your code in order to compute shadows and light falloff.
-
 	vec3f shade = vec3f(0.0, 0.0, 0.0);
 	vec3f N = i.N;
 	vec3f P = r.at(i.t);
-	
-	shade += ke + prod(ka, scene->getAmbientLight());
+
+	vec3f transparency = vec3f(1, 1, 1) - kt;
+	vec3f ambient = prod(ka, scene->getAmbientLight());
+	shade += ke + prod(ambient, transparency);
 
 	for (Scene::cliter j = scene->beginLights(); j != scene->endLights(); j++) {
 		
-		vec3f dir = (*j)->getDirection(P).normalize();
-		vec3f light = (*j)->getColor(P);
-		vec3f diffuse = kd * maximum(N.dot(dir), 0.0);
-		
 		vec3f atten = (*j)->distanceAttenuation(P) * (*j)->shadowAttenuation(P);
+		
+		vec3f dir = (*j)->getDirection(P).normalize();
+		vec3f diffuse = prod(transparency, kd * maximum(N.dot(dir), 0.0));
+		
 		vec3f R = (2 * (dir.dot(N)) * N - dir).normalize();
 		vec3f spec = ks * pow(maximum(R*(-r.getDirection()), 0.0), shininess*128.0);
-		shade += atten * light * (diffuse + spec);
+		shade += prod(atten, diffuse + spec);
 	}
 
 	return shade.clamp();
